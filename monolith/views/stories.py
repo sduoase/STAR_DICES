7835a8ad3787ef5f5b738dd1b5bf81444d3f685a
Stefano Duo <duostefano93@gmail.com>
2019-11-02 23:09:26 +0100

    Check for user and story existence during follow/unfollow and like/dislike procedures.
    Additionally remove redundant liked and disliked info from Like and Dislike db table, they can be retrieved from the liked/disliked story (they refer to the author of the story).

--- monolith/views/stories.py
+++ monolith/views/stories.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, render_template, request
+from flask import Blueprint, redirect, render_template, request, abort
 from monolith.database import db, Story, Like, Dislike
 from monolith.auth import admin_required, current_user
 from flask_login import (current_user, login_user, logout_user,
@@ -37,13 +37,15 @@ def _random_story(message=''):
 @stories.route('/story/<story_id>/like')
 @login_required
 def _like(story_id):
-    q = Like.query.filter_by(liker_id=current_user.id, story_id=story_id)
     story = Story.query.filter_by(id=story_id).first()
+    if story is None:
+        abort(404)
+    
+    q = Like.query.filter_by(liker_id=current_user.id, story_id=story_id)
     if q.first() is None:
         new_like = Like()
         new_like.liker_id = current_user.id
         new_like.story_id = story_id
-        new_like.liked_id = story.author_id
         # remove dislike, if present
         d = Dislike.query.filter_by(disliker_id=current_user.id, story_id=story_id).first()
         if d is not None: 
@@ -58,13 +60,15 @@ def _like(story_id):
 @stories.route('/story/<story_id>/dislike')
 @login_required
 def _dislike(story_id):
-    q = Dislike.query.filter_by(disliker_id=current_user.id, story_id=story_id)
     story = Story.query.filter_by(id=story_id).first()
+    if story is None:
+        abort(404)
+
+    q = Dislike.query.filter_by(disliker_id=current_user.id, story_id=story_id)
     if q.first() is None:
         new_dislike = Dislike()
         new_dislike.disliker_id = current_user.id
         new_dislike.story_id = story_id
-        new_dislike.disliked_id = story.author_id
         # remove like, if present
         l = Like.query.filter_by(liker_id=current_user.id, story_id=story_id).first()
         if l is not None:
