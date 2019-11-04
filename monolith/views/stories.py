5ea70bf1dc9b0614b5d185afa99820d23b43b438
Stefano Duo <duostefano93@gmail.com>
2019-11-04 15:39:20 +0100

    Add test for is_valid_story. Fix case sensitivity issues found after writing tests.

--- monolith/views/stories.py
+++ monolith/views/stories.py
@@ -1,3 +1,5 @@
+import re
+
 from flask import Blueprint, redirect, render_template, request, abort
 from monolith.database import db, Story, Like, Dislike
 from monolith.auth import admin_required, current_user
@@ -123,7 +125,8 @@ def _remove_dislike(story_id):
 # Function to be called during story publishing.
 # If it return False, stop publishing and return an error message.
 def is_story_valid(story_text, dice_roll):
+    split_story_text = re.findall(r"[\w']+|[.,!?;]", story_text.lower())
     for word in dice_roll:
-        if story_text.find(word) == -1:
+        if word.lower() not in split_story_text:
             return False
     return True
