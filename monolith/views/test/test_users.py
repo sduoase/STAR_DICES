38d27021fb2b562fcd529e68f77ccf38b4642942
Stefano Duo <duostefano93@gmail.com>
2019-11-07 16:11:29 +0100

    Add success test for logged in user and failure for nonexistant user stuff.

--- monolith/views/test/test_users.py
+++ monolith/views/test/test_users.py
@@ -49,6 +49,7 @@ class TestAuth(TestHelper):
         reply =  self.client.get('/wall/1/follow')
         self.assertEqual(reply.status_code, 200)
         self.assert_template_used("message.html")
+        self.assert_context("message", "Cannot follow yourself")
 
     def test_login_required_unfollow(self):
         reply = self.client.get('/wall/1/unfollow')
@@ -60,6 +61,7 @@ class TestAuth(TestHelper):
         reply =  self.client.get('/wall/1/unfollow')
         self.assertEqual(reply.status_code, 200)
         self.assert_template_used("message.html")
+        self.assert_context("message", "Cannot unfollow yourself")
 
     def test_login_required_my_followers(self):
         reply = self.client.get('/my_wall/followers')
@@ -71,3 +73,26 @@ class TestAuth(TestHelper):
         reply =  self.client.get('/my_wall/followers')
         self.assertEqual(reply.status_code, 200)
         self.assert_template_used("myfollowers.html")
+
+    def test_nonexistant_public_wall(self):
+        reply = self._login("example@example.com", "admin")
+        self.assertEqual(reply.status_code, 302)
+
+        reply =  self.client.get('/wall/2')
+        self.assertEqual(reply.status_code, 200)
+        self.assert_template_used("message.html")
+        self.assert_context("message", "Ooops.. Writer not found!")
+
+    def test_follow_nonexistant_user(self):
+        reply = self._login("example@example.com", "admin")
+        self.assertEqual(reply.status_code, 302)
+
+        reply =  self.client.get('/wall/2/follow')
+        self.assertEqual(reply.status_code, 404)
+
+    def test_unfollow_nonexistant_user(self):
+        reply = self._login("example@example.com", "admin")
+        self.assertEqual(reply.status_code, 302)
+
+        reply =  self.client.get('/wall/2/unfollow')
+        self.assertEqual(reply.status_code, 404)
